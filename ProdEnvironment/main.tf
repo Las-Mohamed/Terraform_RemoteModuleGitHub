378b6936ef2896da8a66402d95723f3f6d36345c
Las-Mohamed <airscal@gmail.com>
2023-05-31 16:08:51 +0200

    V2

--- ProdEnvironment/main.tf
+++ ProdEnvironment/main.tf
@@ -1,5 +1,5 @@
 module "ChildResources" {
-  source = "../WebServerModule"
+  source = "github.com/Las-Mohamed/Last_Brief13_Terraform//WebServerModule"
   instance_size = "Standard_D3_v2"
   location = "westeurope"
   environment = "Prod"
